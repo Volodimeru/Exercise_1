394ab6522ee0a693ba836498478e1c464afb5936
Volodimeru <126890668+Volodimeru@users.noreply.github.com>
2023-03-19 15:10:46 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 #Creates a string variable and asks user their name, removes whitespaces and capitalizes user's input.
 name = input("Hi, what's yopur name?").strip().capitalize()
+output = ("Hello", name + "," ,"nice to meet you!")
 #Prints greeting with using name inputted by user.
-print ("Hello", name + "," ,"nice to meet you!")
+print (output)
